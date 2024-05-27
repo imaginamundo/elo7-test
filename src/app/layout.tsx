7d7ccebbbfb68cdd8361427bbe67150d1b399dbe
Diogo Fernandes <diogo.ff@icloud.com>
2024-05-27 18:53:22 -0300

    Add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,23 @@
+import localFont from "next/font/local";
+
 import Header from "@/components/Header/Header";
 import "./globals.scss";
+
+const museo = localFont({
+  src: [
+    {
+      path: "../font/MuseoSans/500.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../font/MuseoSans/700.woff2",
+      weight: "700",
+      style: "bold",
+    },
+  ],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={museo.className}>
       <body>
         <Header />
         {children}
